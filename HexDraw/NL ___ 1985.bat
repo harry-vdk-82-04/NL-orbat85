6147fed72b0f04277953b1353f45c723b9869c93
Harry <h.van.der.kooij@deltapi.nl>
2014-10-21 21:38:01 +0200

    First version

---
+++ HexDraw/NL ___ 1985.bat
@@ -0,0 +1,10 @@
+call "NL 37b 1985.bat"
+call "NL 37f 1985.bat"
+
+call "NL 38b 1985.bat"
+call "NL 38f 1985.bat"
+
+call "NL 39b 1985.bat"
+call "NL 39f 1985.bat"
+
+call "NL 40b 1985.bat"
---
+++ HexDraw/NL ___ mapgen.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" %1 -crop   8267x9143+152+71  %2
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" %2 -resize 3733x4192         %3
